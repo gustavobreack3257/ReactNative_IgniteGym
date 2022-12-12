dfc68be8aa975620a2a7f4cd0854225b5e357bf0
Gustavo <guh-51@hotmail.com>
2022-12-12 15:36:04 -0300

    Atualizando

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -3,29 +3,57 @@ import { Button } from "@components/Button";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 
+import * as FileSystem from 'expo-file-system'
 import * as ImagePicker from 'expo-image-picker'
-import { Center, ScrollView, VStack, Skeleton, Text, Heading} from "native-base";
+import { Center, ScrollView, VStack, Skeleton, Text, Heading, useToast} from "native-base";
 import { useState } from "react";
-import { TouchableOpacity } from "react-native";
+import { Alert, TouchableOpacity } from "react-native";
 
 const PHOTO_SIZE = 33
 export function Profile(){
     const [photoIsLoading, setPhotoIsLoading] = useState(false)
     const [userPhoto, setUserPhoto] = useState('https://github.com/gustavobreack3257.png')
 
+    const toast = useToast();
+
     async function handleUserPhotoSelect(){
-        const photoSelected = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
-            quality: 1,
-            aspect: [4, 4],
-            allowsEditing: true
-        });
-        console.log(photoSelected)
-        if(photoSelected.cancelled){
-            return;
+        setPhotoIsLoading(true)
+        try{
+            const photoSelected = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                quality: 1,
+                aspect: [4, 4],
+                allowsEditing: true
+            });
+
+            if(photoSelected.canceled){
+                return;
+            }
+
+            const [image] = photoSelected.assets;
+
+            if(image.uri){
+                const photoInfo = await FileSystem.getInfoAsync(image.uri);
+
+                if(photoInfo.size && (photoInfo.size / 1024 / 1024) > 3){
+                    return toast.show({
+                        title:'Essa imagem é muito grande. Escolha uma de até 3MB',
+                        placement: 'top',
+                        bgColor: 'red.400'
+                    })
+
+                }
+
+                setUserPhoto(image.uri);
+            }
+
+
+        } catch(error){
+            console.log(error)
+        } finally{
+            setPhotoIsLoading(false)
         }
 
-        setUserPhoto(photoSelected.uri);
     }
     return(
         <VStack flex={1}>
